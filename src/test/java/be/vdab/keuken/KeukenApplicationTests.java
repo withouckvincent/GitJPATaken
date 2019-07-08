7dcaeec2c49a4c2b4750d4390f7013a067f2c6f4
withouckvincent <withouck.vincent@gmail.com>
2019-07-08 09:41:03 +0200

    start2

---
+++ src/test/java/be/vdab/keuken/KeukenApplicationTests.java
@@ -0,0 +1,16 @@
+package be.vdab.keuken;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KeukenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
