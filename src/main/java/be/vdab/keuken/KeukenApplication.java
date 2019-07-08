12c83222d6e2d6562a3f1d816e53cff327643eed
withouckvincent <withouck.vincent@gmail.com>
2019-07-08 09:57:22 +0200

    start3

--- src/main/java/be/vdab/keuken/KeukenApplication.java
+++ src/main/java/be/vdab/keuken/KeukenApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KeukenApplication {
-	 
-	 
+	 	 
 	public static void main(String[] args) {
 		SpringApplication.run(KeukenApplication.class, args);
 	}
